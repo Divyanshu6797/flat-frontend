ce7d97de1636449916c37cc736e05bcac2e4bdcf
Divyanshu <117548669+Divyanshu6797@users.noreply.github.com>
2024-11-24 03:47:09 +0530

    Update PropertyList.jsx

--- src/components/PropertyList.jsx
+++ src/components/PropertyList.jsx
@@ -515,6 +515,7 @@ const PropertyList = () => {
 
 
               <p>{property.address}</p>
+              <p>Visited: {property.description}</p>
               <p>Rent: {property.rent}</p>
               <p>Deposit: {property.deposit}</p>
               <p>Furnishing: {property.furnishing}</p>
